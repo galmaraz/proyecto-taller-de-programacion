95868690991c0b483bf6e7c61ebfafb1a04305ab
Galia Almaraz <galia.almaraz@uab.edu.bo>
2024-11-22 12:35:52 -0400

    mejorando el menu

--- create.py
+++ create.py
@@ -5,9 +5,9 @@ class Create:
         self.connection = Database.get_connection()
 
     def nuevo_campeon(self):
-        nombre = input("Nombre del campeón: ")
-        rol = input("Rol del campeón: ")
-        habilidades = input("Habilidades del campeón: ")
+        nombre = input("Nombre del campeón para iniciar la partida: ")
+        rol = input("Rol del campeón:\n ejemplos de roles: \n Carril superior\n Jungla\n Carril central\ Soporte\n Ingrese su rol:  ")
+        habilidades = input("Habilidades del campeón: \n ejemplos de habilidades: \n Garen:\n Pasiva: genera un porcentaje de salud maxima\n Judgmen:Garen gira rápidamente con su espada, infligiendo daño a los enemigos cercanos.\n Ingrese su habilidad:")
 
         cursor = self.connection.cursor()
         query_campeon = "INSERT INTO campeones (nombre, rol, habilidades) VALUES (%s, %s, %s)"
@@ -17,7 +17,7 @@ class Create:
         print(f"¡Campeón creado con ID: {campeon_id}!")
 
         # Crear Ítem relacionado
-        nombre_item = input("Nombre del ítem relacionado: ")
+        nombre_item = input("Nombre del ítem relacionado: \n ejemplo:\nBlade of the Ruined King\nRabadon's Deathcap\n Ingrese el item:")
         tipo_item = input("Tipo del ítem: ")
         costo_item = int(input("Costo del ítem: "))
         query_item = "INSERT INTO items (nombre, tipo, costo, campeon_id) VALUES (%s, %s, %s, %s)"
