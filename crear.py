aa4d5c6c43fd16063f0197819a81efe8c257fd2b
Carlos-G369 <carlosgarciacanaviri@gmail.com>
2024-11-24 13:43:54 -0400

    cambio de algunas varable y agrege una accion en el menu

---
+++ crear.py
@@ -0,0 +1,41 @@
+from database import Database
+
+class Crear:
+    def __init__(self):
+        self.connection = Database.get_connection()
+        # Funcion para Crear un campeon
+    def nuevo_campeon(self):
+        nombre = input("Nombre del campeón para iniciar la partida: ")
+        rol = input("Rol del campeón:\n Carril superior 'Top lane' \n Jungle 'Jungla' \n Carril central 'Mid lane' \n Carril inferior 'Support' \n Carril inferior 'Bot lane' \n Ingrese su rol:  ")
+        habilidades = input("Habilidades del campeón: \n Ejemplos de habilidades: \n Garen:\n Pasiva: genera un porcentaje de salud maxima\n Judgmen: Garen gira rápidamente con su espada, infligiendo daño a los enemigos cercanos.\n Ingrese su habilidad: ")
+
+        cursor = self.connection.cursor()
+        sql_campeon = "INSERT INTO campeones (nombre, rol, habilidades) VALUES (%s, %s, %s)"  
+        cursor.execute(sql_campeon, (nombre, rol, habilidades))
+        self.connection.commit()
+        campeon_id = cursor.lastrowid
+        print(f"¡Campeón creado con ID: {campeon_id}!")
+
+        # Crear Ítem relacionado
+        nombre_item = input("Nombre del item relacionado segun el rol: \n Ejemplo:\n 'Top lane -> Blade of the Ruined King' \n 'Mid lane -> Rabadon's Deathcap' \n 'Bot lane -> Infinity Edge' \n Ingrese el item: ")
+        tipo_item = input("Tipo del item 'Daño fisico, Daño magico, Defensa, Penetracion de armadura': ")
+        costo_item = int(input("Costo del item: "))
+        sql_item = "INSERT INTO items (nombre, tipo, costo, campeon_id) VALUES (%s, %s, %s, %s)"
+        cursor.execute(sql_item, (nombre_item, tipo_item, costo_item, campeon_id))
+        self.connection.commit()
+        print(f"¡Item creado para el campeón {nombre}!")
+
+        # Crear Partida y Relación
+        modo = input("Modo de juego de la partida 'Clasificatoria, Normal, Aram': ")
+        num_jugadores = int(input("Numero de jugadores: "))
+        sql_partida = "INSERT INTO partidas (modo, num_jugadores) VALUES (%s, %s)"
+        cursor.execute(sql_partida, (modo, num_jugadores))
+        self.connection.commit()
+        partida_id = cursor.lastrowid
+        print(f"¡Partida creada exitosamente con ID: {partida_id}!")
+
+        # Relacionar Campeón y Partida
+        sql_relacion = "INSERT INTO campeones_partidas (campeon_id, partida_id) VALUES (%s, %s)"
+        cursor.execute(sql_relacion, (campeon_id, partida_id))
+        self.connection.commit()
+        print(f"¡Campeón {nombre} relacionado con la partida {modo}!")
--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from create import Create
+#Importamos las clases para instanciarlas, y la base de datos
+from crear import Crear
 from read import Read
-from update import Update
+from actualizar import Actualizar
 from eliminar import Delete
 from database import Database
 
 def main():
     # Instanciar las clases
-    create = Create()
+    create = Crear()
     read = Read()
-    update = Update()
+    update = Actualizar()
     delete = Delete()
 
     while True:
         print("\n--- Menú Principal ---")
         print("1. Crear Campeón, Ítem y Partida")
         print("2. Ver Campeones, Ítems y Partidas")
-        print("3. Actualizar Campeón y su Ítem Relacionado y partida ")
-        print("4. Eliminar")
-        print("5. Salir")
+        print("3. Ver Partidas")
+        print("4. Actualizar Campeón y su Ítem Relacionado y partida ")
+        print("5. Eliminar")
+        print("6. Salir")
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
@@ -27,20 +29,22 @@ def main():
             # Leer Campeones, Ítems y Partidas
             read.listar_campeones()
         elif opcion == "3":
+            read.listar_partidas()
+        elif opcion == "4":
             # Actualizar Campeón y su Ítem Relacionado
             update.actualizar_campeon()
 
-        elif opcion == "4":
+        elif opcion == "5":
                 delete.eliminar_campeon()
                 
-        elif opcion == "5":
+        elif opcion == "6":
             # Salir del Programa
             print("¡Hasta luego!")
             Database.close_connection()
             break
 
         else:
-            print("Opción no válida. Por favor, seleccione nuevamente.")
+            print("Opción no válida. Por favor seleccione nuevamente.")
 
 if __name__ == "__main__":
     main()
