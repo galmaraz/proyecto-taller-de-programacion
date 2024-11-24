021fba59a90d2592d30615cba6e8c4490a1ddcb1
Carlos-G369 <carlosgarciacanaviri@gmail.com>
2024-11-24 15:17:19 -0400

    Agrege al menu mas cosas como ver items, ver, partida falta mejorar el actualizar

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ def main():
         print("1. Crear Campeón, Ítem y Partida")
         print("2. Ver Campeones, Ítems y Partidas")
         print("3. Ver Partidas")
-        print("4. Actualizar Campeón y su Ítem Relacionado y partida ")
-        print("5. Eliminar")
-        print("6. Salir")
+        print("4. Ver items")
+        print("5. Actualizar Campeón y su Ítem Relacionado y partida ")
+        print("6. Eliminar campeon")
+        print("7. Eliminar partida")
+        print("8. Eliminar item")
+        print("9. Salir")
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
@@ -29,15 +32,24 @@ def main():
             # Leer Campeones, Ítems y Partidas
             read.listar_campeones()
         elif opcion == "3":
+            #Leer partidas 
             read.listar_partidas()
         elif opcion == "4":
+            #Listar items 
+            read.listar_itmes()
+        elif opcion == "5":
             # Actualizar Campeón y su Ítem Relacionado
             update.actualizar_campeon()
-
-        elif opcion == "5":
-                delete.eliminar_campeon()
-                
         elif opcion == "6":
+            #Elimina al un campeon 
+            delete.eliminar_campeon()
+        elif opcion == "7":
+            #Elimina un partida
+            delete.eliminar_partida()
+        elif opcion == "8":
+            #Elimina un item 
+            delete.eliminar_item()
+        elif opcion == "9":
             # Salir del Programa
             print("¡Hasta luego!")
             Database.close_connection()
